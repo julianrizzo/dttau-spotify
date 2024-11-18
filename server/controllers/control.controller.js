e7acf8257e950e145fd293e03721191f5902d0e7
Julian <jurizzo@deloitte.com.au>
2024-11-18 13:33:10 +1100

    Updated API calls to use axios

--- server/controllers/control.controller.js
+++ server/controllers/control.controller.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const request = require('request');
 const dotenv = require('dotenv');
 
@@ -11,24 +12,26 @@ const resumeMusic = async (req, res) => {
     var options = {
         url: 'https://api.spotify.com/v1/me/player/play' + device_id_params,
         headers: { 'Authorization': 'Bearer ' + getAccessToken() },
-        json: true
+        json: true,
+        method: 'PUT'
     };
 
-    request.put(options, function (error, response, body) {
-        res.send(body);
-    });
+    let response = await axios(options);
+
+    res.send(response.data);
 };
 
 const pauseMusic = async (req, res) => {
     var options = {
         url: 'https://api.spotify.com/v1/me/player/pause' + device_id_params,
         headers: { 'Authorization': 'Bearer ' + getAccessToken() },
-        json: true
+        json: true,
+        method: 'PUT'
     };
 
-    request.put(options, function (error, response, body) {
-        res.send(body);
-    });
+    let response = await axios(options);
+
+    res.send(response.data);
 };
 
 
--- server/controllers/data.controller.js
+++ server/controllers/data.controller.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const request = require('request');
 
 const { getUserID, getAccessToken } = require('../helpers/memory.helper');
@@ -6,39 +7,38 @@ const getUserPlaylists = async (req, res) => {
   var options = {
     url: 'https://api.spotify.com/v1/me/playlists',
     headers: { 'Authorization': 'Bearer ' + getAccessToken() },
-    json: true
+    json: true,
+    method: 'GET'
   };
 
-  request.get(options, function (error, response, body) {
+  let response = await axios(options);
 
-    const playlists = body.items
-      .filter(item => item.owner.id === getUserID())
-      .map(item => ({ name: item.name, id: item.id }));
-    console.log(playlists);
+  const playlists = response.data.items
+    .filter(item => item.owner.id === getUserID())
+    .map(item => ({ name: item.name, id: item.id }));
+  console.log(playlists);
 
-    res.send(playlists);
-  });
+  res.send(playlists);
+  return(playlists);
 };
 
 const getSpotifyPlaylists = async (req, res) => {
-  return new Promise((resolve, reject) => {
 
-    var options = {
-      url: 'https://api.spotify.com/v1/me/playlists',
-      headers: { 'Authorization': 'Bearer ' + getAccessToken() },
-      json: true
-    };
+  var options = {
+    url: 'https://api.spotify.com/v1/me/playlists',
+    headers: { 'Authorization': 'Bearer ' + getAccessToken() },
+    json: true,
+    method: 'GET'
+  };
 
-    request.get(options, function (error, response, body) {
+  let response = await axios(options);
 
-      const playlists = body.items
-        .filter(item => item.owner.display_name === "Spotify")
-        .map(item => ({ name: item.name, id: item.id, owner: item.owner }));
+  const playlists = response.data.items
+    .filter(item => item.owner.display_name === "Spotify")
+    .map(item => ({ name: item.name, id: item.id, owner: item.owner }));
 
-      resolve(playlists);
-      res.send(playlists);
-    });
-  });
+  res.send(playlists);
+  return(playlists);
 };
 
 
@@ -46,19 +46,14 @@ const getDevices = async (req, res) => {
   var options = {
     url: 'https://api.spotify.com/v1/me/player/devices',
     headers: { 'Authorization': 'Bearer ' + getAccessToken() },
-    json: true
+    json: true,
+    method: 'GET'
   };
 
-  request.get(options, function (error, response, body) {
-
-    console.log(body);
-
-    const devices = body.devices;
-    console.log(devices);
-
-    res.json(devices);
-  });
-};
+  let response = await axios(options);
+  res.json(response.data.devices);
+  return(response.data.devices);
+}
 
 module.exports = {
   getUserPlaylists,
